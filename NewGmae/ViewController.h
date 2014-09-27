23a334f128d28ba1d58618aa6c173ddfa816a4c4
Lion User <User@Lions-Mac.local>
2014-09-27 20:24:21 +0100

    Initial Commit

---
+++ NewGmae/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NewGmae
+//
+//  Created by Lion User on 27/09/2014.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ NewGmae/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  NewGmae
+//
+//  Created by Lion User on 27/09/2014.
+//  Copyright (c) 2014 __MyCompanyName__. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+@end
